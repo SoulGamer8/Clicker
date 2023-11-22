760ccb9abdc2879bf5e91f52b7b300db48c15083
SoulGamer <anton3103@outlook.com>
2023-11-22 14:48:50 +0000

    Auto click

--- Assets/_Scripts/ClickManager.cs
+++ Assets/_Scripts/ClickManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,9 +7,20 @@ public class ClickManager : MonoBehaviour
     public UnityAction<int> ClickedEvent; 
 
     [SerializeField] private int _moneyPerClick=1;
-    [SerializeField] private float _moneyPerSecond=0;
+    [SerializeField] private int _moneyPerSecond=5;
     
+    private void Start() {
+        StartCoroutine(MoneyPerSecond());
+    }
+
     public void Click(){
         ClickedEvent?.Invoke(_moneyPerClick);
+    }
+
+    IEnumerator MoneyPerSecond(){
+        while(true){
+            yield return new WaitForSeconds(1);
+             ClickedEvent?.Invoke(_moneyPerSecond);
+        }
     }
 }
